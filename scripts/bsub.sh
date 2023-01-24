342b6084725eba60dfc67be9c5acc3aaffd44aa5
cjtosh <ctosh@cs.ucsd.edu>
2023-01-24 15:59:14 -0500

    bsub

--- scripts/bsub.sh
+++ scripts/bsub.sh
@@ -2,12 +2,10 @@
 
 rm output_logs/*
 
-#####################################
-#### MSE distance ######
-#####################################
 
-bsub -J 'norm_mse[1-50]' -W 1:00 -n 3 -e output_logs/norm_mse%I.err -o output_logs/norm_mse%I.out sh scripts/mse/norm.sh
 
-bsub -J 'norm_row[1-50]' -W 1:00 -n 3 -e output_logs/norm_row%I.err -o output_logs/norm_row%I.out sh scripts/row/norm.sh
+bsub -J 'brenton_mse[1-50]' -W 1:00 -n 3 -e output_logs/brenton_mse%I.err -o output_logs/brenton_mse%I.out sh scripts/mse/brenton.sh
 
-bsub -J 'norm_max[1-50]' -W 1:00 -n 3 -e output_logs/norm_max%I.err -o output_logs/norm_max%I.out sh scripts/max/norm.sh
+bsub -J 'brenton_row[1-50]' -W 1:00 -n 3 -e output_logs/brenton_row%I.err -o output_logs/brenton_row%I.out sh scripts/row/brenton.sh
+
+bsub -J 'brenton_max[1-50]' -W 1:00 -n 3 -e output_logs/brenton_max%I.err -o output_logs/brenton_max%I.out sh scripts/max/brenton.sh
